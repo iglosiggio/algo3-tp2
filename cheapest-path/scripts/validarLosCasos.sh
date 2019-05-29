b44c21b9d6747f1bf699788c540d4230f9ffd61d
federico sabatini <SabatiniFedericoAgustin@gmail.com>
2019-05-29 12:10:27 -0300

    error corregido en validaCasos

--- cheapest-path/scripts/validarLosCasos.sh
+++ cheapest-path/scripts/validarLosCasos.sh
@@ -5,19 +5,15 @@ dijkstra=$(dirname "$0")/../dijkstra.algo
 pqdijkstra=$(dirname "$0")/../pq-dijkstra.algo
 floyd=$(dirname "$0")/../floyd-warshall.algo
 
-
-
 for file in ../casos/*.in; do 
-
-    a=$(md5sum <<< ./$bellman < $file)
-    b=$(md5sum <<< ./$dijkstra < $file)
-    c=$(md5sum <<< ./$pqdijkstra < $file)
-    d=$(md5sum <<< ./$floyd < $file)
+    a=$(./$bellman < $file | md5sum)
+    b=$(./$dijkstra < $file | md5sum)
+    c=$(./$pqdijkstra < $file | md5sum)
+    d=$(./$floyd < $file | md5sum)
 
     if [ "$a" == "$b" ] && [ "$a" == "$c" ] && [ "$a" == "$d" ]; then 
         echo $file "md5sum Check Ok!"
     else
         echo $file "md5sum FAIL!"
     fi;
-
 done
