ff37267051a1ecf3297acc6759e3908daa17152a
Ignacio Losiggio <iglosiggio@gmail.com>
2019-05-29 16:36:04 -0300

    scripts: Emprolijar el script de validar casos

--- cheapest-path/scripts/validarLosCasos.sh
+++ cheapest-path/scripts/validarLosCasos.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-bellman=$(dirname "$0")/../bellman-ford.algo
-dijkstra=$(dirname "$0")/../dijkstra.algo
-pqdijkstra=$(dirname "$0")/../pq-dijkstra.algo
-floyd=$(dirname "$0")/../floyd-warshall.algo
+base=$(dirname "$0")
 
-for file in ../casos/*.in; do 
-    a=$(./$bellman < $file | md5sum)
-    b=$(./$dijkstra < $file | md5sum)
-    c=$(./$pqdijkstra < $file | md5sum)
-    d=$(./$floyd < $file | md5sum)
+bellman=$base/../bellman-ford.algo
+dijkstra=$base/../dijkstra.algo
+pqdijkstra=$base/../pq-dijkstra.algo
+floyd=$base/../floyd-warshall.algo
 
-    if [ "$a" == "$b" ] && [ "$a" == "$c" ] && [ "$a" == "$d" ]; then 
-        echo $file "md5sum Check Ok!"
+for file in "$base"/../casos/*.in; do
+    a=$("$bellman"    < "$file" | md5sum)
+    b=$("$dijkstra"   < "$file" | md5sum)
+    c=$("$pqdijkstra" < "$file" | md5sum)
+    d=$("$floyd"      < "$file" | md5sum)
+
+    if [ "$a" = "$b" ] && [ "$a" = "$c" ] && [ "$a" = "$d" ]; then
+        echo "$file" "md5sum Check Ok!"
     else
-        echo $file "md5sum FAIL!"
+        echo "$file" "md5sum FAIL!"
     fi;
 done
