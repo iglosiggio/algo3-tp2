c006e4f8c7e902d17ff8e324d99198c49a966bbc
Sabatini federico <SabatiniFedericoAgustin@gmail.com>
2019-05-30 04:35:44 -0300

    generadas series expA

--- cheapest-path/scripts/generarSeries.sh
+++ cheapest-path/scripts/generarSeries.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 algo=$1
-ubicacionFile="../series/$algo.series"
+exp=$2
+ubicacionFile="../series/$algo.$exp.series"
 
 s_n=0
 s_m=0
@@ -14,13 +15,22 @@ for filename in $(ls ../data/expA.*.$algo.resultados ) ; do
 
 	while IFS=' ' read -r name n m tiempo 
 	do
-
-        echo $name $n $m $tiempo 
         s_n=$(echo "scale=20; $s_n + $n" | bc)
         s_m=$(echo "scale=20; $s_m + $m" | bc)
-        s_tiempo=$(echo "scale=20; $s_n + $tiempo" | bc)
+        s_tiempo=$(echo "scale=20; $s_tiempo + $tiempo" | bc)
         cant=$(echo $cant + 1 | bc)
 
 	done < "$filename"
 
+	if [ $cant -gt 0 ]; then 
+	    s_n=$(echo "$s_n / $cant" | bc)
+	    s_m=$(echo "$s_m / $cant" | bc)
+	    s_tiempo=$(echo "scale=20; $s_tiempo / $cant" | bc)
+	    echo $s_n $s_m $s_tiempo >> $ubicacionFile
+	fi
+
+    s_n=0
+	s_m=0
+	s_tiempo=0.0
+	cant=0
 done
